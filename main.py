2d3c8a54971db8dbe625ca0b8cf8953a79c25e0a
Joao Clemencio <joao.clemencio@ly.st>
2022-01-13 13:16:19 +0000

    support 4 playlists

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ CLIENT_SECRET = os.environ.get("CLIENT_SECRET").strip()
 DISCOVER_WEEKLY_ID = os.environ.get("DISCOVER_WEEKLY_ID").strip()
 SAVE_TO_ID = os.environ.get("SAVE_TO_ID").strip()
 
+DISCOVER_WK_IDS = [
+  os.environ.get("DISCOVER_WK_ID_0").strip(),
+  os.environ.get("DISCOVER_WK_ID_1").strip(),
+  os.environ.get("DISCOVER_WK_ID_2").strip(),
+  os.environ.get("DISCOVER_WK_ID_3").strip()
+]
+
+DESTINATION_IDS = [
+  os.environ.get("DESTINATION_ID_0").strip(),
+  os.environ.get("DESTINATION_ID_1").strip(),
+  os.environ.get("DESTINATION_ID_2").strip(),
+  os.environ.get("DESTINATION_ID_3").strip()
+]
+
 OAUTH_TOKEN_URL = "https://accounts.spotify.com/api/token"
 def refresh_access_token():
     payload = {
@@ -27,8 +41,8 @@ def refresh_access_token():
     return response.json()
 
 
-def get_playlist(access_token):
-    url = "https://api.spotify.com/v1/playlists/%s" % DISCOVER_WEEKLY_ID
+def get_playlist(access_token, discover_wk_id):
+    url = "https://api.spotify.com/v1/playlists/%s" % discover_wk_id
     headers = {
        "Content-Type": "application/json",
        "Authorization": "Bearer %s" % access_token
@@ -36,8 +50,8 @@ def get_playlist(access_token):
     response = requests.get(url, headers=headers)
     return response.json()
 
-def add_to_playlist(access_token, tracklist):
-    url = "https://api.spotify.com/v1/playlists/%s/tracks" % SAVE_TO_ID
+def add_to_playlist(access_token, tracklist, destination_id):
+    url = "https://api.spotify.com/v1/playlists/%s/tracks" % destination_id
     payload = {
         "uris" : tracklist
     }
@@ -50,20 +64,22 @@ def add_to_playlist(access_token, tracklist):
 
 
 def main():
-    if REFRESH_TOKEN is None or CLIENT_ID is None or CLIENT_SECRET is None or DISCOVER_WEEKLY_ID is None or SAVE_TO_ID is None:
+    if REFRESH_TOKEN is None or CLIENT_ID is None or CLIENT_SECRET is None:
         print("Environment variables have not been loaded!")
         return
 
     access_token = refresh_access_token()['access_token']
-    tracks =  get_playlist(access_token)['tracks']['items']
-    tracklist = []
-    for item in tracks:
-        tracklist.append(item['track']['uri'])
-    response = add_to_playlist(access_token, tracklist)
 
-    if "snapshot_id" in response:
-        print("Successfully added all songs")
-    else:
-        print(response)
+    for i in range(len(DISCOVER_WK_IDS)):
+        tracks =  get_playlist(access_token, DISCOVER_WK_IDS[i])['tracks']['items']
+        tracklist = []
+        for item in tracks:
+            tracklist.append(item['track']['uri'])
+        response = add_to_playlist(access_token, tracklist, DESTINATION_IDS[i])
+
+        if "snapshot_id" in response:
+            print("Successfully added all songs")
+        else:
+            print(response)
 
 main()
